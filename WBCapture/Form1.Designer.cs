c22d29aadb82453bf4d2b3dedbe94b313948ad4c
creeperzzzz <creeper.zzz@gmail.com>
2022-10-26 01:27:15 +0900

    ver0.01

---
+++ WBCapture/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WBCapture
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.button1 = new System.Windows.Forms.Button();
+			this.chkTop = new System.Windows.Forms.CheckBox();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.textInterval = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(430, 397);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.textInterval);
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Controls.Add(this.chkTop);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(430, 80);
+			this.panel1.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(138, 13);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(104, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "表示領域決定";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// chkTop
+			// 
+			this.chkTop.AutoSize = true;
+			this.chkTop.Location = new System.Drawing.Point(155, 49);
+			this.chkTop.Name = "chkTop";
+			this.chkTop.Size = new System.Drawing.Size(60, 16);
+			this.chkTop.TabIndex = 0;
+			this.chkTop.Text = "最前面";
+			this.chkTop.UseVisualStyleBackColor = true;
+			this.chkTop.CheckedChanged += new System.EventHandler(this.chkTop_CheckedChanged);
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.pictureBox1);
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel2.Location = new System.Drawing.Point(0, 80);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(430, 397);
+			this.panel2.TabIndex = 2;
+			// 
+			// textInterval
+			// 
+			this.textInterval.Location = new System.Drawing.Point(77, 47);
+			this.textInterval.Name = "textInterval";
+			this.textInterval.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.textInterval.Size = new System.Drawing.Size(45, 19);
+			this.textInterval.TabIndex = 2;
+			this.textInterval.Text = "1000";
+			this.textInterval.TextChanged += new System.EventHandler(this.textInterval_TextChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 50);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(59, 12);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "更新間隔：";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(13, 18);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(119, 12);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "1000,1000 / 1000,1000";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(430, 477);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.Name = "Form1";
+			this.Text = "指定領域を一定間隔でキャプチャするやつ";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.CheckBox chkTop;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textInterval;
+	}
+}
+
---
+++ WBCapture/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WBCapture
+{
+	public partial class Form1 : Form
+	{
+		Timer m_timer;
+		Rectangle m_rect;
+		Bitmap m_bitmap;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			m_timer = new Timer();
+			m_timer.Tick += timer_Tick;
+
+			if (Properties.Settings.Default.WindowPosition != new Point(7575, 7575))
+			{
+				Location = Properties.Settings.Default.WindowPosition;
+				Size = Properties.Settings.Default.WindowSize;
+			}
+			m_rect = new Rectangle(
+				Properties.Settings.Default.CapturePosition,
+				Properties.Settings.Default.CaptureSize);
+			m_timer.Interval = Properties.Settings.Default.Interval;
+			chkTop.Checked = Properties.Settings.Default.TopMost;
+
+			m_timer.Start();
+
+			updateRect();
+		}
+
+		private void timer_Tick(object sender, EventArgs e)
+		{
+			using (Graphics g = Graphics.FromImage(m_bitmap))
+			{
+				g.CopyFromScreen(m_rect.Location, new Point(0, 0), m_rect.Size);
+			}
+			//Invalidate();
+			pictureBox1.Invalidate();
+		}
+
+		private void Form1_Paint(object sender, PaintEventArgs e)
+		{
+			e.Graphics.DrawImage(m_bitmap, Point.Empty);
+		}
+
+		private void chkTop_CheckedChanged(object sender, EventArgs e)
+		{
+			TopMost = chkTop.Checked;
+		}
+
+		private void updateRect()
+		{
+			m_bitmap = new Bitmap(m_rect.Width, m_rect.Height);
+			pictureBox1.Image = m_bitmap;
+
+			label2.Text = string.Format("{0}, {1} / {2}, {3}", m_rect.Left, m_rect.Top, m_rect.Width, m_rect.Height);
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			m_rect = pictureBox1.RectangleToScreen(new Rectangle(Point.Empty, pictureBox1.Size));
+			updateRect();
+		}
+
+		private void textInterval_TextChanged(object sender, EventArgs e)
+		{
+			try
+			{
+				m_timer.Interval = Convert.ToInt32(textInterval.Text);
+			}
+			catch
+			{
+			}
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			Properties.Settings.Default.Interval = m_timer.Interval;
+			Properties.Settings.Default.WindowPosition = this.Location;
+			Properties.Settings.Default.WindowSize = this.Size;
+			Properties.Settings.Default.CapturePosition = m_rect.Location;
+			Properties.Settings.Default.CaptureSize = m_rect.Size;
+			Properties.Settings.Default.TopMost = this.TopMost;
+			Properties.Settings.Default.Save();
+		}
+	}
+}
